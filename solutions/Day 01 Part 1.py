ce6fa9b2dd6a0d95213852177b0cb1665927272b
trigtbh <lucas.varotto01@gmail.com>
2024-11-30 21:06:44 -0800

    horrible start but day 1 complete

--- solutions/Day 01 Part 1.py
+++ solutions/Day 01 Part 1.py
@@ -1,6 +1,15 @@
 import aoc
-contents = aoc.get_input(2024, 1)
+contents = aoc.get_input(2024, 1).strip()
 del aoc
 
 # ---
 
+lines = contents.split("\n")
+l1 = sorted([int(l.split(" ")[0]) for l in lines])
+l2 = sorted([int(l.split(" ")[3]) for l in lines])
+
+x = 0
+for i in range(len(l1)):
+    x += abs(l1[i] - l2[i])
+
+print(x)
--- solutions/Day 01 Part 2.py
+++ solutions/Day 01 Part 2.py
@@ -1,5 +1,16 @@
 import aoc
-contents = aoc.get_input(2024, 1)
+contents = aoc.get_input(2024, 1).strip()
 del aoc
 
 # ---
+
+lines = contents.split("\n")
+l1 = ([int(l.split(" ")[0]) for l in lines])
+l2 = ([int(l.split(" ")[3]) for l in lines])
+
+x = 0
+for i in range(len(l1)):
+    x += l1[i] * l2.count(l1[i])
+
+
+print(x)
