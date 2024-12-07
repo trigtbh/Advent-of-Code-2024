9dc23507fb4073768e7454747f5dbb58dc92b952
trigtbh <lucas.varotto01@gmail.com>
2024-12-06 21:12:24 -0800

    part 1 tripped me up so much omfg

---
+++ solutions/Day 07 Part 1.py
@@ -0,0 +1,23 @@
+import aoc
+contents = aoc.get_input(2024, 7).strip()
+del aoc
+# ---
+
+def rec(n, nums, target):
+    if n > target: return False
+    if len(nums) == 0:
+        return n == target
+    
+    return rec(n * nums[0], nums[1:], target) or rec(n + nums[0], nums[1:], target)
+
+
+x = 0
+for line in contents.split("\n"):
+    target, nums = line.split(": ")
+    target = int(target); nums = list(map(int, nums.split(" ")))
+
+    if rec(nums[0], nums[1:], target):
+
+        x += target
+
+print(x)
